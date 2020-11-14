7cf30133552380ee14f040b24f44a78578829ce8
RoyMcCrain <matsuda.r@kanamekey.com>
2020-11-15 00:36:53 +0900

    Add

--- keyboards/keyboardio/atreus/keymaps/roy/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/roy/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,     KC_8,    KC_9,    KC_0,
-    UNDO,    CUT,     COPY,    PSTE,    KC_LPRN, _______, _______, KC_RPRN, KC_CH,    KC_CQ,  KC_ASTR, KC_PLUS,
+    KC_TAB,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINUS,_______, _______, KC_RGUI, KC_CH,    KC_CQ,   KC_ASTR, KC_PLUS,
     _______, _______, _______, _______, _______, _______, _______, KC_CENT, _______,  KC_DEL,  _______, _______
 ),
 
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT(
     _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
     _______, _______, KC_BTN2, KC_BTN1, _______,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______,
+    UNDO,    CUT,     COPY,    PSTE,    _______, _______, _______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
--- keyboards/lily58/keymaps/roy/keymap.c
+++ keyboards/lily58/keymaps/roy/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
   KC_LGUI, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LALT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
-                             KC_ESC, KC_LCTL,KC_LSFT, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
+                             KC_ESC, KC_LCTL,KC_LSFT, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RCTL \
   ),
 
   [_LOWER] = LAYOUT( \
