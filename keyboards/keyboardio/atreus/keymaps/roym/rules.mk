e5dbd6012f6ebd0815f889d03b3e9b88a1584bb6
RoyMcCrain <matsuda.r@kanamekey.com>
2023-02-07 13:18:43 +0900

    Add atreus

---
+++ keyboards/keyboardio/atreus/keymaps/roym/rules.mk
@@ -0,0 +1,15 @@
+MOUSEKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+EXTRAFLAGS += -flto
+LTO_ENABLE = yes
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+UNICODE_ENABLE = yes        # Unicode
+
