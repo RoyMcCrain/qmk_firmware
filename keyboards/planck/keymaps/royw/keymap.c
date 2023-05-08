c03777f510237f1adf3a3f37803436fb0ee1c750
RoyMcCrain <matsuda.r@kanamekey.com>
2023-05-09 01:58:45 +0900

    Update

--- keyboards/planck/keymaps/royw/keymap.c
+++ keyboards/planck/keymaps/royw/keymap.c
@@ -9,6 +9,7 @@ enum planck_layers {
     _LOWER,
     _RAISE,
     _ADJUST,
+    _NAGINATA_RAISE
 };
 
 enum planck_keycodes {
@@ -32,11 +33,15 @@ enum planck_keycodes {
 // ctl+[ (esc)
 #define AESC RCS(KC_LBRC)
 #define V_SV LSFT(KC_V)
-#define V_CV LCTL(KC_V)
+#define V_CJ LCTL(KC_J)
 #define V_CD LCTL(KC_D)
 #define V_CU LCTL(KC_U)
 // Mission Control
 #define MC LGUI(KC_TAB)
+// レイヤー切り替え
+#define N_R MO(_NAGINATA_RAISE)
+#define N_LEFT LSFT(KC_LEFT)
+#define N_RGHT LSFT(KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -64,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |   1  |   2  |   3  |   4  |   5  |      |      |   6  |   7  |   8  |   9  |   0  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |  ;q  |  ;w  |  ZZ  |      |      | C V  |  S v |      |      |      |
+   * |      |      |  ;q  |  ;w  |  ZZ  |      |      | C j  |  S v |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |  ENT |      | DEL  |      |      |
    * `-----------------------------------------------------------------------------------'
@@ -72,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_planck_grid(
       _______, PSTE,    UNDO,    COPY,    _______,  _______, _______, _______, _______, _______, _______,  _______,
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     _______, _______, KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
-      _______, _______, V_Q,     V_W,     V_WQ,     _______, _______, V_CV,    V_SV,    _______, _______,  _______,
+      _______, _______, V_Q,     V_W,     V_WQ,     _______, _______, V_CJ,    V_SV,    _______, _______,  _______,
       _______, _______, _______, _______, _______,  _______, GENT,    GENT,    _______, KC_DEL,  _______,  _______
   ),
 
@@ -91,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, TPBM,    RTLF,
       KC_BSLS, KC_GRV,  KC_EQL,  KC_SLSH, KC_MINUS, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
       _______, _______, KC_UNDS, KC_LPRN, KC_LBRC,  _______, _______, KC_RBRC, KC_RPRN, V_CD,    V_CU,    _______,
-      _______, _______, AESC,    _______, KC_TAB,   KC_TAB,  _______, _______, _______, _______, _______, _______
+      _______, AESC,    _______, _______, KC_TAB,   KC_TAB,  _______, _______, _______, _______, _______, _______
   ),
 
   /* Adjust (Lower + Raise)
@@ -106,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_ADJUST] = LAYOUT_planck_grid(
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   RESET,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
       _______, _______, KC_BTN2, KC_BTN1, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
       KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  _______, _______, _______, MC,      _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -116,7 +121,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       NG_Q,    NG_W,    NG_E,    NG_R,    NG_T,    _______, _______,  NG_Y,     NG_U,    NG_I,    NG_O,    NG_P,
       NG_A,    NG_S,    NG_D,    NG_F,    NG_G,    _______, _______,  NG_H,     NG_J,    NG_K,    NG_L,    NG_SCLN,
       NG_Z,    NG_X,    NG_C,    NG_V,    NG_B,    _______, _______,  NG_N,     NG_M,    NG_COMM, NG_DOT,  NG_SLSH,
-      _______, _______, _______, _______, NG_SHFT, NG_SHFT, NG_SHFT2, NG_SHFT2, _______, _______, _______, _______
+      _______, _______, _______, _______, NG_SHFT, NG_SHFT, NG_SHFT2, NG_SHFT2, N_R,     _______, _______, _______
+  ),
+
+  [_NAGINATA_RAISE] = LAYOUT_planck_grid(
+      _______, PSTE,    UNDO,    COPY,    _______,  _______, _______, _______, _______, _______, _______,  _______,
+      _______, _______, _______, N_LEFT,  N_RGHT,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
+      _______, _______, V_Q,     V_W,     V_WQ,     _______, _______, V_CJ,    V_SV,    _______, _______,  _______,
+      _______, _______, _______, _______, _______,  _______, _______, _______, _______, KC_DEL,  _______,  _______
   )
 
 };
